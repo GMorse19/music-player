bc0c0316856ec291d341741b4d7ddf4d09fb2a83
GMorse19 <malorse@comcast.net>
2020-07-13 00:08:10 -0400

    Initial set-up for player

--- src/MusicPlayer/Music.js
+++ src/MusicPlayer/Music.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Music.css'
 import rock from './SchoolOfRock.m4a'
 
 const Music = () => {
+  const [track, setTrack] = useState('')
+
+  useEffect(() => {
+    setTrack(rock)
+  }, [])
+
+  console.log(track)
+
   return (
     <div className='container'>
       <p>Music Player</p>
-      <audio className='music-player' controls>
-        <source src={rock} type='audio/mpeg'/>
-      </audio>
+      <audio id='player' src={track} controls />
     </div>
   )
 }
