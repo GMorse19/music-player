1979f0544af2b2ada4e24b5281c5b3fe93fc7d13
GMorse19 <malorse@comcast.net>
2020-07-13 22:04:30 -0400

    Added list

--- src/MusicPlayer/Music.js
+++ src/MusicPlayer/Music.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import './Music.css'
 import rock from './SchoolOfRock.m4a'
+import pledge from './Pledge.m4a'
 
 const Music = () => {
   const [track, setTrack] = useState('')
@@ -10,12 +11,22 @@ const Music = () => {
     setTrack(rock)
   }, [])
 
-  console.log(track)
+  const list = [{ id: 1, title: "SchoolOfRock", url: rock }, {id: 2, title: "Booting Up", url: pledge }].map(item => {
+        return (
+          <li
+            key={item.id}
+            onClick={() => setTrack(item.url)}
+          >
+          {item.title}
+          </li>
+        )
+      })
 
   return (
     <div className='container'>
       <p>Music Player</p>
       <audio id='player' src={track} controls />
+      {list}
     </div>
   )
 }
